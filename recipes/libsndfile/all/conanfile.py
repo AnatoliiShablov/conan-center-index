73354241c2f280560b5fb271d07f03a42f3b43d3
Шаблов Анатолий Владимирович <43937468+AnatoliiShablov@users.noreply.github.com>
2025-11-19 10:57:00 +0300

    Newer mpg123 for libsndfile if available

--- recipes/libsndfile/all/conanfile.py
+++ recipes/libsndfile/all/conanfile.py
@@ -77,7 +77,7 @@ class LibsndfileConan(ConanFile):
             self.requires("flac/1.4.2")
             self.requires("opus/[>=1.4 <2]")
         if self.options.get_safe("with_mpeg"):
-            self.requires("mpg123/1.31.2")
+            self.requires("mpg123/[>=1.31.2 <2]")
             self.requires("libmp3lame/3.100")
 
     def source(self):
